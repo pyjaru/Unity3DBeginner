84bcd85830f8ee0f3cdfd0dc05e4a1b3514853d7
Yongmin Choi <pyjaru@gmail.com>
2019-05-28 19:37:45 +0900

    유니티 3D 비기너 스터디 2주차
    - 게임 엔딩까지 완료

---
+++ Assets/Scripts/GameEnding.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEnding : MonoBehaviour
+{
+    public float fadeDuration = 1f;
+    public float displayImageDuration = 1f;
+    public GameObject player;
+    public CanvasGroup exitBackgroundImageCanvasGroup;
+
+    private bool m_IsPlayerAtExit;
+    private float m_Timer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_IsPlayerAtExit)
+        {
+            print("EndLevel");
+            EndLevel();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            m_IsPlayerAtExit = true;
+        }
+    }
+
+    private void EndLevel()
+    {
+        m_Timer += Time.deltaTime;
+
+        exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
+
+        if (m_Timer > fadeDuration + displayImageDuration)
+        {
+            Application.Quit();
+        }
+    }
+}
---
+++ Assets/Scripts/NavigationTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavigationTest : MonoBehaviour
+{
+    NavMeshAgent agent;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            //ray.hit
+            //agent.SetDestination();
+        }
+    }
+}
